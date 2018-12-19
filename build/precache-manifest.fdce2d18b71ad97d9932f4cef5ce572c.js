d7fb6d47eab6ac2925f14d6c170e6d15d6199d55
plugshubham <shbhkumar6@gmail.com>
2018-12-19 09:46:24 +0530

    first commit

---
+++ build/precache-manifest.fdce2d18b71ad97d9932f4cef5ce572c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4db23ab13928e7efedec",
+    "url": "/static/js/main.4db23ab1.chunk.js"
+  },
+  {
+    "revision": "77fd409d06f9538fc4ed",
+    "url": "/static/js/1.77fd409d.chunk.js"
+  },
+  {
+    "revision": "4db23ab13928e7efedec",
+    "url": "/static/css/main.4799c5e7.chunk.css"
+  },
+  {
+    "revision": "77fd409d06f9538fc4ed",
+    "url": "/static/css/1.61d14a46.chunk.css"
+  },
+  {
+    "revision": "a215e1cec486479c34837b12b87d1771",
+    "url": "/index.html"
+  }
+];
